1412bee44f20bb57233f4d1caa51a9a62276e4eb
Timofey Voronov <timmycarousel@gmail.com>
2023-01-31 17:05:07 +0300

    проверить перед отправкой

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,24 @@ let page = document.querySelector('.page');
 let editButton = page.querySelector('.profile__edit-button');
 let popup = page.querySelector('.popup');
 let closeIcon = page.querySelector('.popup__close-icon');
-let popupContainer = page.querySelector('.popup__container');
+let popupField = page.querySelector('.popup__field');
 let nameInput = page.querySelector('.popup__field-name');
 let infoInput = page.querySelector('.popup__field-info');
-
 let name = page.querySelector('.profile-info__name');
 let info = page.querySelector('.profile-info__text');
 
 
-
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    let name = page.querySelector('.profile-info__name');
-    let info = page.querySelector('.profile-info__text');
+
     info.textContent = infoInput.value;
     name.textContent = nameInput.value;
-    popup.classList.remove('popup_active');
+    displayNone();
 }
-popupContainer.addEventListener('submit', handleFormSubmit);
+popupField.addEventListener('submit', handleFormSubmit);
 
 
 function displayFlex() {
-
-    let name = page.querySelector('.profile-info__name');
-    let info = page.querySelector('.profile-info__text');
     nameInput.value = name.textContent;
     infoInput.value = info.textContent;
     popup.classList.add('popup_active');
