916135f6a4b69f2ef6fc394bbcda3eb3778a7642
Timofey Voronov <timmycarousel@gmail.com>
2023-05-24 11:36:51 +0300

    ПР

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Section } from "../components/Section.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
 import { UserInfo } from "../components/UserInfo.js";
-import { initialCards, validationConfig } from "../scripts/Data.js";
+import { validationConfig } from "../scripts/Data.js";
 import "./index.css";
 import { Api } from "../components/Api.js";
 import { PopupWithConfirmation } from "../components/PopupWithConfirmation.js";
